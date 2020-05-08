1352dae920728e8428f9e6a91f9cdc8171421238
Kaminskyi <Kaminskyjj270396@gmail.com>
2020-05-08 09:45:38 +0300

    Зааончил д/з по 1-23

--- 1-23/app.js
+++ 1-23/app.js
@@ -222,3 +222,53 @@ const newCharArr = charArr.reduce((acc, value) => {
 }, []);
 
 // console.log(newCharArr);
+
+// 5. Отсортируйте массив массивов так, чтобы вначале располагались наименьшие массивы
+// (размер массива определяется его длиной): [  [14, 45],  [1],  ['a', 'c', 'd']  ] → [ [1], [14, 45], ['a', 'c', 'd'] ]
+
+const arr3 = [[14, 45], [1], ["a", "c", "d"]];
+
+arr3.sort((prev, next) => prev.length - next.length);
+
+// console.log(arr3);
+
+// 6. Есть массив объектов:
+const coreArr = [
+  { cpu: "intel", info: { cores: 2, сache: 3 } },
+  { cpu: "intel", info: { cores: 4, сache: 4 } },
+  { cpu: "amd", info: { cores: 1, сache: 1 } },
+  { cpu: "intel", info: { cores: 3, сache: 2 } },
+  { cpu: "amd", info: { cores: 4, сache: 2 } },
+];
+
+// Отсортировать их по возрастающему количеству ядер (cores).
+
+coreArr.sort((prev, next) => prev.info.cores - next.info.cores);
+
+// console.log(coreArr);
+
+// 7. Создать функцию, которая будет принимать массив продуктов и две цены.
+// Функция должна вернуть все продукты, цена которых находится в указанном диапазоне,
+// и сортировать от дешевых к дорогим:
+
+let products = [
+  { title: "prod1", price: 5.2 },
+  { title: "prod2", price: 0.18 },
+  { title: "prod3", price: 15 },
+  { title: "prod4", price: 25 },
+  { title: "prod5", price: 18.9 },
+  { title: "prod6", price: 8 },
+  { title: "prod7", price: 19 },
+  { title: "prod8", price: 63 },
+];
+
+// filterCollection(products, 15, 30) → [{...price: 15}, {...price: 18.9}, {...price: 19}, {...price: 25}]
+
+function filterProduct(arr, min, max) {
+  const products = arr.filter(
+    (price) => price.price >= min && price.price <= max
+  );
+  return products.sort((prev, next) => prev.price - next.price);
+}
+
+// console.log(filterProduct(products, 15, 30));
