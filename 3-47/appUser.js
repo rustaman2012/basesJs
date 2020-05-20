11a921b70d0d189335020bd7c56dfe0f4790847a
Kaminskyi <Kaminskyjj270396@gmail.com>
2020-05-20 16:47:00 +0300

    AJAX

---
+++ 3-47/appUser.js
@@ -0,0 +1,116 @@
+// Получить пользователей (users) от сервера https://jsonplaceholder.typicode.com. Получив ответ от сервера вывести имена пользователей на страницу. При клике на имя пользователя в произвольном месте должна появиться подробная информация о нем. Для визуальной части можно использовать bootstrap или другие фреймворки.
+
+const btn = document.getElementById("btn");
+const container = document.querySelector(".container");
+
+function getUsers(cb) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+  xhr.send();
+  xhr.addEventListener("load", (e) => {
+    const response = JSON.parse(xhr.response);
+    cb(response);
+  });
+}
+
+btn.addEventListener("click", (e) => {
+  getUsers(renderUserForHtml);
+});
+
+function renderUserForHtml(response) {
+  const fragment = document.createDocumentFragment();
+
+  response.forEach((user) => {
+    const card = document.createElement("div");
+    card.classList.add(
+      "card",
+      "text-white",
+      "bg-primary",
+      "p-2",
+      "bd-highlight",
+      "mb-3"
+    );
+
+    card.style.maxWidth = "400px";
+    const cardHeader = document.createElement("div");
+    cardHeader.classList.add("card-header");
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+
+    const cardText = document.createElement("p");
+    cardText.classList.add("card-text");
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+
+    card.appendChild(cardHeader);
+    card.appendChild(cardBody);
+
+    cardHeader.textContent = `Login: ${user.username}, Name: ${user.name}, Email: ${user.email}`;
+
+    const { address } = user;
+
+    cardTitle.textContent = "Адрес";
+
+    cardText.appendChild(renderAddressForHtml(address));
+
+    fragment.appendChild(card);
+  });
+  container.classList.add(
+    "d-flex",
+    "bd-highlight",
+    "mb-3",
+    "flex-wrap",
+    "justify-content-around"
+  );
+  container.appendChild(fragment);
+}
+
+function renderAddressForHtml({ street, suite, city, zipcode }) {
+  const randomColor = randomColorClass();
+
+  const ul = document.createElement("ul");
+  ul.classList.add("list-group");
+
+  const liStreet = document.createElement("li");
+  liStreet.classList.add("list-group-item", randomColor);
+  liStreet.textContent = street;
+
+  const liSuite = document.createElement("li");
+  liSuite.classList.add("list-group-item", randomColor);
+  liSuite.textContent = suite;
+
+  const liCity = document.createElement("li");
+  liCity.classList.add("list-group-item", randomColor);
+  liCity.textContent = city;
+
+  const liZipcode = document.createElement("li");
+  liZipcode.classList.add("list-group-item", randomColor);
+  liZipcode.textContent = zipcode;
+
+  ul.appendChild(liStreet);
+  ul.appendChild(liSuite);
+  ul.appendChild(liCity);
+  ul.appendChild(liZipcode);
+
+  return ul;
+}
+
+function randomColorClass() {
+  const arrColor = [
+    "primary",
+    "secondary",
+    "success",
+    "danger",
+    "warning",
+    "info",
+    "light",
+    "dark",
+  ];
+  const randomValue = Math.floor(Math.random() * arrColor.length);
+  return "list-group-item-" + arrColor[randomValue];
+}
